f20cffe2004748ce9aeb6ec0935710be6224f9c1
Vladislav <zherihovvlad@gmail.com>
2021-02-23 08:55:29 +0300

    project init

---
+++ src/main/java/zherikhov/goProject/GoProjectApplication.java
@@ -0,0 +1,13 @@
+package zherikhov.goProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zherikhov/goProject/GoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package zherikhov.goProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
